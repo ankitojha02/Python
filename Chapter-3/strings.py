369809381bd2a78dfb8195ed036a70741d6c1328
ankitojha02 <ojhaa2207@gmail.com>
2026-06-24 18:17:52 +0530

    Intro to strings in python

---
+++ Chapter-3/strings.py
@@ -0,0 +1,11 @@
+a = "Hello, World!"
+print(a)  # Output: Hello, World!
+
+b = a.upper()
+print(b)  # Output: HELLO, WORLD!
+
+c = a[3:8] #slice the string from index 3 to 7 (8 is not included)
+print(c)  # Output: lo, W
+
+d = len(a)
+print(d)  # Output: 13
