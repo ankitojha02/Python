5843bdf0a5112fc3df646bd4a027eb19a3a7a7a2
ankitojha02 <ojhaa2207@gmail.com>
2026-06-24 18:46:03 +0530

    escape sequence

---
+++ Chapter-3/escape_seq.py
@@ -0,0 +1,3 @@
+a = "Hello,\n\"World!"  # Using double quotes inside the string
+print(a)  # Output: Hello, "World"!
+
--- Chapter-3/strings.py
+++ Chapter-3/strings.py
@@ -9,3 +9,22 @@ print(c)  # Output: lo, W
 
 d = len(a)
 print(d)  # Output: 13
+
+e = a[-1]  # Access the last character of the string
+print(e)  # Output: !
+
+print(a[:4]) # Output: Hell (slice from start to index 3)
+
+#skipping characters in a string
+f = a[1:7:2]
+print(f)  # Output: el, (slice from index 1 to 6, skipping every 2nd character)
+
+#built-in string methods
+g = a.replace("World", "Python")
+print(g)  # Output: Hello, Python!
+print(a.endswith("!"))  # Output: True
+
+print(a.startswith("Hello"))  # Output: True
+
+name = "Alice"
+print(f"Hello, {name}!")  # Output: Hello, Alice! (using f-string for string formatting
