ad8cd6afc1cfdd72b012887c36be213ce6010afb
ankitojha02 <ojhaa2207@gmail.com>
2026-06-24 18:11:38 +0530

    practice set

---
+++ Chapter-2/input.py
@@ -0,0 +1,7 @@
+a = input("Enter a number: ")
+print(a)
+print(type(a))  # Output: <class 'str'>
+
+b = int(input("Enter another number: "))
+print(b)
+print(type(b))  # Output: <class 'int'>
---
+++ Chapter-2/practice.py
@@ -0,0 +1,27 @@
+a = 4
+b = 6
+print(a + b)  # Output: 10
+
+#Q2
+a = 13
+b = 2
+print(a % b)  # Output: 1
+
+#Q3
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+print(a + b)  # Output: Sum of a and b
+
+#Q4
+a = 37
+b = 23
+print(a > b)  # Output: True
+
+#Q5
+i = int(input("Enter a number: "))
+j = int(input("Enter another number: "))
+print((i + j) / 2)  # Output: Average of i and j
+
+#Q6
+a = 5
+print(a ** 3)  # Output: 125
