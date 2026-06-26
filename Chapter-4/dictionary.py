4c890ca2d0bdaed1755aaee35679dd444f873397
ankitojha02 <ojhaa2207@gmail.com>
2026-06-26 13:57:32 +0530

    Intro to dictionary

---
+++ Chapter-4/dictionary.py
@@ -0,0 +1,8 @@
+marks = {
+    "A": 90,
+    "B": 80,
+    "C": 70,
+}
+
+print(marks)  # Output: {'A': 90, 'B': 80, 'C': 70}
+print(type(marks))  # Output: <class 'dict'>
--- Chapter-4/tuples.py
+++ Chapter-4/tuples.py
@@ -2,3 +2,9 @@
 a = (1, 2, 3)
 b = (4, 5, 6)
 print(type(a))  # Output: <class 'tuple'>
+
+#Built-in functions
+a.count(2)  # Count the number of occurrences of 2 in the tuple
+print(a.count(2))  # Output: 1
+a.index(3)  # Find the index of the first occurrence of 3 in the tuple
+print(a.index(3))  # Output: 2
