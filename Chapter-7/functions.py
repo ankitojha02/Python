f3c7072e61009ff93c5367899b1fbe18e48cef43
ankitojha02 <ojhaa2207@gmail.com>
2026-06-29 15:28:58 +0530

    Intro to recursion in python

--- Chapter-7/functions.py
+++ Chapter-7/functions.py
@@ -1,9 +1,21 @@
-def average(a, b, c):
-    return (a + b + c) / 3
+# def average(a, b, c):
+#     return (a + b + c) / 3
 
-x = 10
-y = 20
-z = 30
+# x = 10
+# y = 20
+# z = 30
 
-print(f"The average of {x}, {y}, and {z} is: {average(x, y, z)}")
+# print(f"The average of {x}, {y}, and {z} is: {average(x, y, z)}")
 
+# def greet(name):
+#     return f"Hello, {name}"
+
+# name = input("Enter your name: ")
+# print(greet(name))
+
+#default argument
+def greet(name="Guest"):
+    return f"Hello, {name}"
+
+print(greet())  # Uses default argument
+print(greet("Alice"))  # Uses provided argument
