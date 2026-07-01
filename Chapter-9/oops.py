963a44ed0d52ea65d84bb21b9f8357e8a76b53dd
ankitojha02 <ojhaa2207@gmail.com>
2026-07-01 14:14:37 +0530

    self keyword in python in oops

--- Chapter-9/oops.py
+++ Chapter-9/oops.py
@@ -1,7 +1,29 @@
-class Student:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# class Student:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
 
-    def display(self):
-        print(f"Name: {self.name}, Age: {self.age}")
+#     def display(self):
+#         print(f"Name: {self.name}, Age: {self.age}")
+
+# class employee:
+   
+#     language = "Python"
+#     salary = 50000
+
+# harry = employee()
+# print(harry.salary)
+
+
+#self is a reference to the current instance of the class. It is used to access variables that belong to the class.
+
+class Student:
+    language = "Python"
+    marks = 80
+
+    def getInfo(self):
+        print(f"Language: {self.language}, Marks: {self.marks}")
+
+# Create an instance of the Student class
+student1 = Student()
+student1.getInfo()
