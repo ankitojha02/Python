e93434caedca94a9a133cfeb44803894ac7a2b0d
ankitojha02 <ojhaa2207@gmail.com>
2026-07-01 14:18:42 +0530

    constructor in python

--- Chapter-9/oops.py
+++ Chapter-9/oops.py
@@ -17,13 +17,25 @@
 
 #self is a reference to the current instance of the class. It is used to access variables that belong to the class.
 
+# class Student:
+#     language = "Python"
+#     marks = 80
+
+#     def getInfo(self):
+#         print(f"Language: {self.language}, Marks: {self.marks}")
+
+# # Create an instance of the Student class
+# student1 = Student()
+# student1.getInfo()
+
+# constructor is a special method in Python classes that is automatically called when an object of the class is created. It is used to initialize the attributes of the class. The constructor method is defined using the `__init__` method.
 class Student:
-    language = "Python"
-    marks = 80
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-    def getInfo(self):
-        print(f"Language: {self.language}, Marks: {self.marks}")
+    def display(self):
+        print(f"Name: {self.name}, Age: {self.age}")
 
-# Create an instance of the Student class
-student1 = Student()
-student1.getInfo()
+setudent1 = Student("Alice", 20)
+setudent1.display()  # Output: Name: Alice, Age: 20
